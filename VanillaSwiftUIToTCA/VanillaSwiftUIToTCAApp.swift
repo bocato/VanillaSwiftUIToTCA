492e568893ecbe942ade732c0ade6ce1013fea6b
Eduardo Bocato <dubocato@gmail.com>
2022-01-28 11:26:01 +0100

    Initial Commit

---
+++ VanillaSwiftUIToTCA/VanillaSwiftUIToTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  VanillaSwiftUIToTCAApp.swift
+//  VanillaSwiftUIToTCA
+//
+//  Created by Bocato, Eduardo on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct VanillaSwiftUIToTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
